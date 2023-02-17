b9d83215a639eb6820c498beeb9f08d6ef8e992e
cpslabttu <esarplab@gmail.com>
2023-02-17 14:47:01 -0800

    Created an Example Class

---
+++ ExampleCode.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ExampleCode {
+    public static void main(String args[]) {
+      Scanner sc= new Scanner(System.in);
+      int a= sc.nextInt();
+      int b= sc.nextInt();
+      
+      System.out.println(sum(a,b));
+    }
+    
+    public static int sum(int a, int b){
+        // write Summation code
+    }
+    
+    public static int sub(int a, int b){
+        // write Subtraction code
+    }
+    
+    public static int mul(int a, int b){
+        // write Multiplication code
+    }
+    
+    public static double div(int a, int b){
+        // write Division code
+    }
+    
+}
